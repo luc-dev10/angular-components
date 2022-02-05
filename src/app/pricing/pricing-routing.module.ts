8e5fef802f7535b52c65428280b98c533aa49988
luc-dev10 <luc.dev1008@gmail.com>
2022-02-05 12:30:19 -0500

    pricing route

--- src/app/pricing/pricing-routing.module.ts
+++ src/app/pricing/pricing-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PricingHomeComponent } from './pricing-home/pricing-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pricing',
+    component: PricingHomeComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class PricingRoutingModule { }
+export class PricingRoutingModule {}
