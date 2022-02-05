c2ba81b84590ee2a887d849acfd5813a7c814ea5
luc-dev10 <luc.dev1008@gmail.com>
2022-02-05 11:39:13 -0500

    configure home in app

--- src/app/typo/typo.module.ts
+++ src/app/typo/typo.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TypoRoutingModule } from './typo-routing.module';
-
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    TypoRoutingModule
-  ]
+  declarations: [HomeComponent],
+  imports: [CommonModule, TypoRoutingModule],
+  exports: [HomeComponent],
 })
-export class TypoModule { }
+export class TypoModule {}
