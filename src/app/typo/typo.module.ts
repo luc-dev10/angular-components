e101a3779dbcf2b39ab2737bd0f9640be2201635
luc-dev10 <luc.dev1008@gmail.com>
2022-02-05 11:16:52 -0500

    start routing

---
+++ src/app/typo/typo.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TypoRoutingModule } from './typo-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TypoRoutingModule
+  ]
+})
+export class TypoModule { }
