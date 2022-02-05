3eb0c54fd05c0a8fb0ea1b81463f88b3d3c187e5
luc-dev10 <luc.dev1008@gmail.com>
2022-02-05 11:45:38 -0500

    remove the home component

--- src/app/typo/typo.module.ts
+++ src/app/typo/typo.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { TypoRoutingModule } from './typo-routing.module';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [HomeComponent],
   imports: [CommonModule, TypoRoutingModule],
-  exports: [HomeComponent],
 })
 export class TypoModule {}
