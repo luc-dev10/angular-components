25d136aebcfc6d471fd9f523958ed1bf29d08203
luc-dev10 <luc.dev1008@gmail.com>
2022-02-07 21:55:38 -0500

    shared component

--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ServicesRoutingModule } from './services-routing.module';
 import { ServicesHomeComponent } from './services-home/services-home.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [ServicesHomeComponent],
-  imports: [CommonModule, ServicesRoutingModule],
+  imports: [CommonModule, ServicesRoutingModule, SharedModule],
 })
 export class ServicesModule {}
