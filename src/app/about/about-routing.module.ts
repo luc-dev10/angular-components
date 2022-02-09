aff9f953a5d8c207f49bc21a2e28542b716d69bf
luc-dev10 <luc.dev1008@gmail.com>
2022-02-09 16:20:15 -0500

    add advanced routing

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -1,11 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutHomeComponent } from './about-home/about-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AboutHomeComponent,
+    children: [
+      {
+        path: 'biography',
+        component: BiographyComponent,
+      },
+      {
+        path: 'companies',
+        component: CompaniesComponent,
+      },
+      {
+        path: 'partners',
+        component: PartnersComponent,
+      },
+    ],
   },
 ];
 
--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutHomeComponent } from './about-home/about-home.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
 
 @NgModule({
-  declarations: [AboutHomeComponent],
+  declarations: [AboutHomeComponent, BiographyComponent, CompaniesComponent, PartnersComponent],
   imports: [CommonModule, AboutRoutingModule],
 })
 export class AboutModule {}
